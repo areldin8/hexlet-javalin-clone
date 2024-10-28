530bfbd2e7496486682ecb365d161971ae68976e
areldin8 <belphegor26ra@gmail.com>
2024-10-29 01:06:48 +0300

    john

--- app/src/main/java/org/example/hexlet/HelloWorld.java
+++ app/src/main/java/org/example/hexlet/HelloWorld.java
@@ -9,7 +9,15 @@ public class HelloWorld {
             config.bundledPlugins.enableDevLogging();
         });
         // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/hello", ctx -> {
+                    var name = ctx.queryParam("name");
+            if (name != null && !name.isEmpty()) {
+                ctx.result("Hello, " + name + "!");
+            } else {
+                ctx.result("Hello, World!");
+            }
+                });
+        app.post("users", ctx -> ctx.result("POST /users"));
         app.start(7070); // Стартуем веб-сервер
     }
 }
